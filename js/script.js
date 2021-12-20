42038b17b96c7811aa7482aa8788c72d1c67e498
Johana <jm7110975@gmail.com>
2021-12-20 13:24:20 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let correo = document.getElementById("correo");
+let contraseña = document.getElementById("contraseña");
+let rcontraseña = document.getElementById("rcontraseña");
+let ingresar = document.getElementById("ingresar-btn");
+let datos = [];
